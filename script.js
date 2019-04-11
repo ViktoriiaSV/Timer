d580e7b81a913df2b117a6881efd92e3230f8d3e
ViktoriiaSV <viktoriiasytnyk@gmail.com>
2019-04-11 14:05:09 +0300

    change

--- script.js
+++ script.js
@@ -1,46 +1,70 @@
-const container = document.querySelector("#attention-container");
+const container = document.querySelector("#container");
 
-class  Notify {
+class Timer {
 
-constructor (content, type) {
-    this.lifeTime = 5000;
-    this.content = content;
-    this.type = type;
-    this.render();
-}
+    constructor(content, type) {
+        this.lifeTime = 10000;
+        this.content = content;
+        this.render();
+    }
 
-createStart (){
-    this.start = document.createElement("button");
-    this.start.classList.add("button-start");
-    this.start.innerHTML = "Start";
-    this.start.addEventListener("click", this.hideNotify);
-    return this.start;
-}
+    createStart() {
+        this.start = document.createElement("button");
+        this.start.classList.add("button-start");
+        this.start.innerHTML = "Start";
+        this.start.addEventListener("click", this.hideNotify);
+        return this.start;
+    }
 
-createLine (){
-  this.line = document.createElement("div");
-  this.line.classList.add("line");
-  return this.line;
+    createLine() {
+        this.line = document.createElement("div");
+        this.line.classList.add("line");
+        return this.line;
+    }
 
-}
+    pauseInterval () {
+clearInterval(this.interval);
+    }
 
-hideNotify () {
+    continueInterval () {
+this.lifeInterval();
+    }
 
-}
+    lifeInterval() {
+        this.interval = setInterval(() => {
+            const currentWidth = this.line.offsetWidth;
+            const percent = (this.width / 100);
+            if (currentWidth - percent < 0) {
+              this.hideLine();
+return;
+            }
+            this.line.style.width = `${currentWidth - percent}px`;
+        }, 100);
+    }
 
-render (){
-    this.notify = document.createElement("div");
-this.notify.classList.add("notify");
-this.notify.innerHTML = this.content;
-// this.notify.classList.add("");
-container.append(this.notify);
-this.notify.append(this.createStart());
-this.notify.append(this.createLine());
+    hideLine() {
+        setTimeout( () => {
+            this.line.remove();
+        },300)
+    }
+
+    render() {
+        this.timer = document.createElement("div");
+        this.timer.classList.add("timer");
+        this.timer.innerHTML = this.content;
+        container.append(this.timer);
+        this.timer.append(this.createStart());
+        this.timer.append(this.createLine());
+        container.append(this.timer);
+        this.timer.addEventListener("mouseenter", this.pauseInterval.bind(this));
+        this.timer.addEventListener("mouseleave", this.continueInterval.bind(this));
+        this.width = this.line.offsetWidth;
+        this.lifeInterval();
 
     }
 
 }
 
-new Notify("Test");
-new Notify("Test 2");
+new Timer("Test");
+new Timer("Test 2");
 
